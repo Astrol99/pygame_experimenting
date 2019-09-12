cdbd0aaf3a9164d7708b3a258bad2703168e05bd
David <dngames2006@gmail.com>
2019-09-11 20:12:16 -0500

    Some starter stuff to do in pygame

--- first_game/blobGame.py
+++ first_game/blobGame.py
@@ -1,2 +1,15 @@
 import pygame
+pygame.init()   # Function that starts lib
 
+win = pygame.display.set_mode((500,500))    # Window resolution
+pygame.display.set_caption("Blob Game")     # Set title of game window (top left)
+
+# Positions of object
+x = 50      
+y = 50
+
+# Size of object
+width = 40 
+height = 60 
+
+vel = 5     # Velocity of object moving
